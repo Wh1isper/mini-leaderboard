cb417cb60fc7dc75c388c26263a0953769fb94f4
Zhongsheng Ji <9573586@qq.com>
2025-05-13 21:28:43 +0800

    feat: Add messageboard (#2)

--- mini_leaderboard/orm.py
+++ mini_leaderboard/orm.py
@@ -25,3 +25,23 @@ class Leaderboard(Base):
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+
+
+class MessageBoard(Base):
+    __tablename__ = "message_board"
+
+    id_ = Column(Integer, autoincrement=True, primary_key=True)
+
+    message_id = Column(
+        Text,
+        nullable=False,
+        index=True,
+        default=lambda: uuid.uuid4().hex,
+        unique=True,
+    )
+    project_id = Column(Text)
+    name = Column(Text)
+    message = Column(Text)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
--- mini_leaderboard/routers/api/v1/__init__.py
+++ mini_leaderboard/routers/api/v1/__init__.py
@@ -1,3 +1,4 @@
 from .leaderboard import router as leaderboard
+from .messgaeboard import router as messageboard
 
-routers = [leaderboard]
+routers = [leaderboard, messageboard]
